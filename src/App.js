13a01318788db6eb8a84a1f17ffc6179c0e89e8a
nathalie7980 <londonhippos@gmail.com>
2022-09-09 16:00:47 +0800

    implement api

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 import EmailForm from "./components/EmailForm";
 
-
 const variants = {
   open: { scale: 0.8, y: "-50%" },
   closed: { scale: 1 },
@@ -13,21 +12,79 @@ const valid = {
   closed: { opacity: 0 },
 };
 
-function App() {
-
+function App() {  
   const [isOpen, setIsOpen] = useState(false);
-  const [isValid, setIsValid] = useState(true)
-console.log(isOpen)
+  const [isValid, setIsValid] = useState(true);
+  const [emailData, setEmailData] = useState({
+    catchAll: "",
+    disposable: "",
+    dns: "",
+    format: "",
+  });
+
   return (
     <div className="mainContainer">
       <motion.nav animate={isOpen ? "open" : "closed"} variants={variants}>
-        <EmailForm isOpen={isOpen} setIsOpen={setIsOpen} setIsValid={setIsValid}/>
+        <EmailForm
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          setIsValid={setIsValid}
+          setEmailData={setEmailData}
+        />
       </motion.nav>
 
-      <div className={`${isOpen ? 'z-0' : ''} validContainer`}>
-        <motion.nav animate={isOpen ? "open" : "closed"} variants={valid} initial={{ opacity: 0 }}>
-          <h1 className={`${isValid ? 'text-emerald-300' : 'text-red-400'} isValid`}>
-            {isValid ? "EMAIL IS VALID" : "EMAIL IS NOT VALID"}</h1>
+      <div className={`${isOpen ? "z-0" : ""} validContainer`}>
+        <motion.nav
+          animate={isOpen ? "open" : "closed"}
+          variants={valid}
+          initial={{ opacity: 0 }}
+        >
+          <h1
+            className={`${isValid ? "isValidText" : "isNotValidText"} isValid`}
+          >
+            {isValid ? "EMAIL IS VALID" : "EMAIL IS NOT VALID"}
+          </h1>
+          <div className="validInfoContainer">
+            <h1>
+              Catch-All Email:{" "}
+              <span
+                className={`${
+                  emailData.catchAll === "false"
+                    ? "text-red-400"
+                    : "text-blue-300"
+                }`}
+              >
+                {emailData.catchAll}
+              </span>
+            </h1>
+            <h1>Disposable Email: <span
+                className={`${
+                  emailData.catchAll === "false"
+                    ? "text-red-400"
+                    : "text-blue-300"
+                }`}
+              >
+                {emailData.disposable}
+              </span></h1>
+            <h1>DNS Check: <span
+                className={`${
+                  emailData.catchAll === "false"
+                    ? "text-red-400"
+                    : "text-blue-300"
+                }`}
+              >
+                {emailData.dns}
+              </span></h1>
+            <h1>Format Check: <span
+                className={`${
+                  emailData.catchAll === "false"
+                    ? "text-red-400"
+                    : "text-blue-300"
+                }`}
+              >
+                {emailData.format}
+              </span></h1>
+          </div>
         </motion.nav>
       </div>
     </div>
--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,21 +1,68 @@
 import React, { useState } from "react";
-import "./App.css"
+import "./App.css";
 
-const EmailForm = ({ isOpen, setIsOpen, setIsValid }) => {
+const EmailForm = ({ isOpen, setIsOpen, setIsValid, setEmailData }) => {
   const [email, setEmail] = useState("");
 
   const onChangeHandler = (e) => {
     setEmail(e.target.value);
   };
 
-  const validateHandler = (email) => {
-    let validRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  
+  // const validateHandler = (email) => {
+  //   let validRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-    if (email.match(validRegex)) {
-      setIsValid(true);
+  //   if (email.match(validRegex)) {
+  //     setIsValid(true);
+  //   } else {
+  //     setIsValid(false);
+  //   }
+  // };
+
+  const axios = require("axios");
+
+  const onclickHandler = (e) => {
+    e.preventDefault();
+    
+    if (email.length >= 1) {
+      setIsOpen(!isOpen);
+      if (!isOpen) {
+        validateHandler(email);
+      }
     } else {
-      setIsValid(false);
+      alert("Please enter an email address.");
     }
+  };
+
+  const validateHandler = (email) => {
+    axios
+      .get(
+        `https://emailverification.whoisxmlapi.com/api/v2?apiKey=${process.env.REACT_APP_API_KEY}&emailAddress=${email}`
+      )
+      .then((response) => {
+        console.log(response.data);
+        const { catchAllCheck, disposableCheck, dnsCheck, formatCheck } =
+          response.data;
+
+        if (
+          catchAllCheck == "false" &&
+          disposableCheck == "false" &&
+          dnsCheck == "true" &&
+          formatCheck == "true"
+        )
+          setIsValid(true);
+        else setIsValid(false);
+
+        setEmailData({
+          catchAll: catchAllCheck,
+          disposable: disposableCheck,
+          dns: dnsCheck,
+          format: formatCheck,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
@@ -31,19 +78,13 @@ const EmailForm = ({ isOpen, setIsOpen, setIsValid }) => {
           name="email"
           spellCheck={false}
           autoFocus={true}
-          className="emailInput"
+          className={`${
+            isOpen ? "pointer-events-none" : "pointer-events-auto"
+          } emailInput`}
           onChange={onChangeHandler}
         />
         <div className="flex justify-center">
-          <button
-            type="submit"
-            onClick={(e) => {
-              e.preventDefault();
-              setIsOpen(!isOpen);
-              validateHandler(email);
-            }}
-            className="emailBtn"
-          >
+          <button type="submit" onClick={onclickHandler} className="emailBtn">
             {!isOpen ? "VALIDATE" : "TRY WITH A DIFFERENT EMAIL"}
           </button>
         </div>
